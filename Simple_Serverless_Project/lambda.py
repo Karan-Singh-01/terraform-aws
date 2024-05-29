9348b57925a210b86cd6e95d36990066e1969445
Karan-Singh-01 <157451190+Karan-Singh-01@users.noreply.github.com>
2024-05-29 13:17:31 +0530

    Update lambda.py

--- Simple_Serverless_Project/lambda.py
+++ Simple_Serverless_Project/lambda.py
@@ -1,3 +1,20 @@
 def lambda_handler(event, context):
     
     return 'statusCode: 200, Hello from Lambda!'
+
+
+
+##################################################################################################
+
+# In case you are using proxy integration then use this code, check out my medium blog to learn more:
+# 
+    
+#import json
+
+#def lambda_handler(event, context):
+#    message = {'message': 'Hello From Lambda!'}
+#    return {
+#        'statusCode': 200,
+#        'headers': {'Content-Type': 'application/json'},
+#        'body': json.dumps(message),
+#    }
