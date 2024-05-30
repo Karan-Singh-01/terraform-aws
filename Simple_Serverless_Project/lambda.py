8763b0b039ba0d66e754109d3730452223ba803c
Karan-Singh-01 <157451190+Karan-Singh-01@users.noreply.github.com>
2024-05-30 09:31:20 +0530

    Update lambda.py

--- Simple_Serverless_Project/lambda.py
+++ Simple_Serverless_Project/lambda.py
@@ -8,7 +8,9 @@ def lambda_handler(event, context):
 
 # In case you are using API Gateway proxy integration with Lambda then use this code, check out my medium blog to learn more:
 # https://medium.com/@karansingh1550/deploy-serverless-with-terraform-conquer-common-api-gateway-lambda-challenges-e4375a2169eb
-    
+
+# Remove the hash marks (#) from the below code:
+
 #import json
 
 #def lambda_handler(event, context):
