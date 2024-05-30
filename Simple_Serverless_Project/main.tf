5cf28a1b451882b89f07fabd79a2383ebc81d0db
Karan-Singh-01 <157451190+Karan-Singh-01@users.noreply.github.com>
2024-05-30 11:37:32 +0530

    Update main.tf

--- Simple_Serverless_Project/main.tf
+++ Simple_Serverless_Project/main.tf
@@ -23,7 +23,7 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "api_resource"
+  path_part   = "api_resource"  ## Use {api_resource+} for proxy integration ##
 }
 
 resource "aws_api_gateway_method" "api_method" {
@@ -39,7 +39,7 @@ resource "aws_api_gateway_method" "api_method" {
 
 resource "aws_api_gateway_integration" "api_integration" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-  type                    = "AWS"
+  type                    = "AWS"   ## Use "AWS_PROXY" for proxy integration ##
   resource_id             = aws_api_gateway_resource.api_resource.id
   http_method             = aws_api_gateway_method.api_method.http_method
   integration_http_method = "POST"
